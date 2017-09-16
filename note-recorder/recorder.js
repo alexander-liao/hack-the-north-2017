ad77bf12e6cf6123d4fb889d1e9661d489a43840
Ethan White <ethanwhite@rogers.com>
2017-09-16 16:01:46 -0400

    Fixed some note recorder stuff

--- note-recorder/recorder.js
+++ note-recorder/recorder.js
@@ -4,6 +4,7 @@ class Note {
     constructor(parsed, startTime) {
         this.parsed = parsed;
         this.startTime = startTime;
+        this.duration = null;
     }
     end(endTime) {
         this.endTime = endTime;
@@ -22,8 +23,8 @@ class Recorder {
         this.startTime = null;
         this.activeNotes = [];
     }
-    getNoteByPitch(pitch) {
-        for(var note of this.notes) {
+    getActiveNoteByPitch(pitch) {
+        for(var note of this.activeNotes) {
             if(note.parsed["pitch"] == pitch) {
                 return note;
             }
@@ -36,9 +37,14 @@ class Recorder {
         var currentTime = +new Date - this.startTime;
         var parsed = JSON.parse(line);
         if(parsed["type"].toLowerCase() == "note on") {
-            this.notes.push(new Note(parsed, currentTime / 1000));
+            var note = new Note(parsed, currentTime / 1000);
+            this.notes.push(note);
+            this.activeNotes.push(note);
         } else if(parsed["type"].toLowerCase() == "note off") {
-            this.getNoteByPitch(parsed["pitch"]).end(currentTime / 1000);
+            var note = this.getActiveNoteByPitch(parsed["pitch"]);
+            // console.log(parsed["pitch"], this.activeNotes.map(note => note.parsed["pitch"]))
+            note.end(currentTime / 1000);
+            this.activeNotes.splice(this.activeNotes.indexOf(note), 1);
         }
     }
     getTempo() {
