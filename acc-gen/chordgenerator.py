926682ad1a25c6856e4c4d75e7f4232873d21c46
alexander-liao <alex.yj.liao@gmail.com>
2017-09-16 14:22:15 -0400

    add test input; "complete" gen; complete preprocessor

--- acc-gen/chordgenerator.py
+++ acc-gen/chordgenerator.py
@@ -8,19 +8,48 @@
 
 import json
 from chordoffsets import *
+from sys import stdin
 
 def match(progression, notes):
     chords = progression["chords"]
     if len(chords) > len(notes):
         return -1 # Don't match partial
     chorddata = [data[chord["type"]] for chord in chords]
-    return progression["weight"] * sum(
-        4 if note["pitch"] in chord[0]
-        else 2 if note["pitch"] in chord[1]
-        else 0 for note, chord in zip(notes, chorddata)
-    )
+    weight = 0
+    for block, chord in zip(notes, chorddata):
+        inner = 0
+        for note in block:
+            if note["pitch"] in chord[0]:
+                inner += 4
+            elif note["pitch"] in chord[1]:
+                inner += 1
+        weight += inner / len(block)
+    return progression["weight"] * weight
 
-def generate(progressions, notes):
-    index = 0
-    while index < len(notes):
-        pass
+selections = 3
+
+def find(progressions, notes): # Top 3
+    result = []
+    for progression in progressions:
+        score = match(progression, notes)
+        if score != -1:
+            chords = progression["chords"]
+            result.append((score, chords, notes[:len(chords)]))
+    return sorted(result)[:selections]
+
+def generate(progressions, notes): # Top 3
+    result = []
+    for config in find(progressions, notes):
+        for right in generate(progressions, notes[len(config[1]):]):
+            score = config[0] + right[0]
+            chords = config[1] + right[1]
+            notes = config[2] + right[2]
+            result.append((score, chords, notes))
+    return sorted(result)[:selections]
+
+print(json.dumps({
+    "blocks": generate(
+        json.loads(open("progressions.json", "r").read()),
+        json.loads(stdin.read())
+    )
+}, indent = 4))
--- acc-gen/preprocessor.py
+++ acc-gen/preprocessor.py
@@ -8,6 +8,12 @@
 import json
 from sys import stdin, stdout
 
+def snap(notes):
+    tempo = notes["tempo"] * 4
+    for note in notes["notes"]:
+        note["startTime"] = round(note["startTime"] * tempo) / tempo
+    return notes
+
 def process(notes):
     uuid = 0
     for note in notes["notes"]:
@@ -15,9 +21,24 @@ def process(notes):
         note.update(uuid = uuid, octaves = note["pitch"] // 12)
         note["pitch"] %= 12
         uuid += 1
-    return notes
+    print(notes)
+    return sorted(notes["notes"], key = lambda note: note["startTime"])
 
 def merge(notes):
     combos = []
+    beat = 0
+    tempo = notes["tempo"]
+    index = 0
+    notelist = notes["notes"]
+    while beat <= notelist[-1]["startTime"]:
+        combo = []
+        while index <= len(notelist):
+            if notelist[index]["startTime"] < beat + tempo:
+                combo.append(notelist[index])
+            else:
+                beat += tempo
+                break
+        combos.append(combo)
+    return combos
 
-print(json.dumps(process(json.loads(stdin.read()))))
+print(json.dumps(process(snap(json.loads(stdin.read()))), indent = 4))
