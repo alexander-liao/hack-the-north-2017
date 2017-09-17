0337ee3326e67aa46db6c2d4bed45cf43490d09b
alexander-liao <alex.yj.liao@gmail.com>
2017-09-17 10:53:15 -0400

    whoops I did goof

--- recorder-conglomerate/harmnz.sh
+++ recorder-conglomerate/harmnz.sh
@@ -2,5 +2,5 @@
 
 # run this from hack-the-north-2017
 
-python3 acc-gen/preprocessor.py | python3 acc-gen/chordgenerator.py "prog-new" | python3 acc-gen/interprocessor.py | python3 acc-gen/satb.py | python3 acc-gen/selector.py 0
+python3 ../acc-gen/preprocessor.py | python3 ../acc-gen/chordgenerator.py "prog-new" | python3 ../acc-gen/interprocessor.py | python3 ../acc-gen/satb.py | python3 ../acc-gen/selector.py 0
 # echo hi
