852a4bfbd9aae31e51554c23654457b03959e499
Ethan White <ethanwhite@rogers.com>
2017-09-17 00:30:38 -0400

    Made harmnz.sh follow PEP 394

--- recorder-conglomerate/harmnz.sh
+++ recorder-conglomerate/harmnz.sh
@@ -1,1 +1,1 @@
-python ../acc-gen/preprocessor.py | python ../acc-gen/chordgenerator.py | python ../acc-gen/interprocessor.py | python ../acc-gen/satb.py | python ../acc-gen/selector.py
+python3 ../acc-gen/preprocessor.py | python3 ../acc-gen/chordgenerator.py | python3 ../acc-gen/interprocessor.py | python3 ../acc-gen/satb.py | python3 ../acc-gen/selector.py
